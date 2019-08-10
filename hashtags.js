ac94bc156e47332f82a33b7c28a58b958a8ae215
Lonely Dutchhound <lonely.dutchhound@gmail.com>
2019-08-11 02:00:55 +0300

    .toString, .substr

--- hashtags.js
+++ hashtags.js
@@ -3,8 +3,11 @@ let hashtagsArray=[];
 
 let wordsArray = tweet.split(' ');
 for (let value of wordsArray){
-  if (value[0]==='#') hashtagsArray.push(value);
+  if (value[0]==='#') {
+    value=value.substr(1);
+    hashtagsArray.push(value)
+  };
 }
 console.log(hashtagsArray);
-let stringOfHashtags = hashtagsArray.join(' ');
+let stringOfHashtags = hashtagsArray.toString();
 alert (stringOfHashtags);
